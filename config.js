bc1dc098be1d34b7068e37c3bf2a28f2af815e85
blake97190 <132440675+blake97190@users.noreply.github.com>
2024-07-11 17:07:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 export const config = {
-  domainSource: "https://www.martialmap.com", // Your WeWeb app link
-  metaDataEndpoint: "https://x7ya-rkul-ymbs.f2.xano.io/api:tQNSyF-Q/weweb/events/metadata/{eventID}", // Link of the endpoint that returns the metadata. /{id} will be added to this path. With id being the last parameter of the dynamic page
-  patterns: {
-    dynamicPage: "/event/[^/]+"
-  }
-};
-export const config = {
     domainSource: "https://www.martialmap.com", // Your WeWeb app link
     patterns: [
         {
