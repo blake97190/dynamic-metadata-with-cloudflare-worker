872bd0d7c550e6942811c67de7251d005d597887
blake97190 <132440675+blake97190@users.noreply.github.com>
2024-07-11 16:59:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,3 +5,17 @@ export const config = {
     dynamicPage: "/event/[^/]+"
   }
 };
+export const config = {
+    domainSource: "https://www.martialmap.com", // Your WeWeb app link
+    patterns: [
+        {
+            pattern: "/event/[^/]+",
+            metaDataEndpoint: "https://x7ya-rkul-ymbs.f2.xano.io/api:tQNSyF-Q/weweb/events/metadata/{eventID}"
+        },
+        {
+            pattern: "/club/[^/]+",
+            metaDataEndpoint: "https://another-api.example.com/meta/{id}"
+        }
+        // Add more patterns and their metadata endpoints as needed
+    ]
+};  
